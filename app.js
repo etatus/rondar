b7e96f628a9dbf0febf27db845d33d898c5e3bc7
etatus <etatus@gmail.com>
2019-04-09 14:57:25 +0200

    Minor fixes

---
+++ app.js
@@ -0,0 +1,340 @@
+
+/**
+ * Calculates and displays a walking route from the St Paul's Cathedral in London
+ * to the Tate Modern on the south bank of the River Thames
+ *
+ * A full list of available request parameters can be found in the Routing API documentation.
+ * see:  http://developer.here.com/rest-apis/documentation/routing/topics/resource-calculate-route.html
+ *
+ * @param   {H.service.Platform} platform    A stub class to access HERE services
+ */
+function calculateRouteFromAtoB (platform) {
+  var router = platform.getRoutingService(),
+    routeRequestParams = {
+      mode: 'shortest;pedestrian',
+      representation: 'display',
+      waypoint0: '42.430847884649,-8.648626030161', 
+      waypoint1: '42.434915884995,-8.643584001564', 
+      waypoint2: '42.431764617121,-8.648568734382', 
+      routeattributes: 'waypoints,summary,shape,legs',
+      maneuverattributes: 'direction,action'
+    };
+
+
+  router.calculateRoute(
+    routeRequestParams,
+    onSuccess,
+    onError
+  );
+}
+/**
+ * This function will be called once the Routing REST API provides a response
+ * @param  {Object} result          A JSONP object representing the calculated route
+ *
+ * see: http://developer.here.com/rest-apis/documentation/routing/topics/resource-type-calculate-route.html
+ */
+function onSuccess(result) {
+  var route = result.response.route[0];
+ /*
+  * The styling of the route response on the map is entirely under the developer's control.
+  * A representitive styling can be found the full JS + HTML code of this example
+  * in the functions below:
+  */
+  addRouteShapeToMap(route);
+  addManueversToMap(route);
+
+  addWaypointsToPanel(route.waypoint);
+  addManueversToPanel(route);
+  addSummaryToPanel(route.summary);
+  // ... etc.
+}
+
+/**
+ * This function will be called if a communication error occurs during the JSON-P request
+ * @param  {Object} error  The error message received.
+ */
+function onError(error) {
+  alert('Ooops!');
+}
+
+
+
+
+/**
+ * Boilerplate map initialization code starts below:
+ */
+
+
+//Step 1: initialize communication with the platform
+var platform = new H.service.Platform({
+  app_id: '<HERE APP ID>',
+  app_code: '<HERE APP CODE>',
+  useCIT: true,
+  useHTTPS: true
+});
+
+var mapContainer;
+var defaultLayers;
+var map;
+var behavior;
+var ui;
+
+/* // set up containers for the map  + panel
+var mapContainer = document.getElementById('map'),
+  routeInstructionsContainer = document.getElementById('panel');
+
+var defaultLayers = platform.createDefaultLayers();
+
+//Step 2: initialize a map - this map is centered over Berlin
+var map = new H.Map(mapContainer,
+  defaultLayers.normal.map,{
+  center: {lat:52.5160, lng:13.3779},
+  zoom: 13
+});
+
+//Step 3: make the map interactive
+// MapEvents enables the event system
+// Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+// Create the default UI components
+var ui = H.ui.UI.createDefault(map, defaultLayers);
+ */
+
+// Hold a reference to any infobubble opened
+var bubble;
+
+/**
+ * Opens/Closes a infobubble
+ * @param  {H.geo.Point} position     The location on the map.
+ * @param  {String} text              The contents of the infobubble.
+ */
+function openBubble(position, text){
+ if(!bubble){
+    bubble =  new H.ui.InfoBubble(
+      position,
+      // The FO property holds the province name.
+      {content: text});
+    ui.addBubble(bubble);
+  } else {
+    bubble.setPosition(position);
+    bubble.setContent(text);
+    bubble.open();
+  }
+}
+
+
+/**
+ * Creates a H.map.Polyline from the shape of the route and adds it to the map.
+ * @param {Object} route A route as received from the H.service.RoutingService
+ */
+function addRouteShapeToMap(route){
+  var strip = new H.geo.Strip(),
+    routeShape = route.shape,
+    polyline;
+
+  routeShape.forEach(function(point) {
+    var parts = point.split(',');
+    strip.pushLatLngAlt(parts[0], parts[1]);
+  });
+
+  polyline = new H.map.Polyline(strip, {
+    style: {
+      lineWidth: 4,
+      strokeColor: 'rgba(0, 128, 255, 0.7)'
+    }
+  });
+  // Add the polyline to the map
+  map.addObject(polyline);
+  // And zoom to its bounding rectangle
+  map.setViewBounds(polyline.getBounds(), true);
+}
+
+
+/**
+ * Creates a series of H.map.Marker points from the route and adds them to the map.
+ * @param {Object} route  A route as received from the H.service.RoutingService
+ */
+function addManueversToMap(route){
+  var svgMarkup = '<svg width="18" height="18" ' +
+    'xmlns="http://www.w3.org/2000/svg">' +
+    '<circle cx="8" cy="8" r="8" ' +
+      'fill="#1b468d" stroke="white" stroke-width="1"  />' +
+    '</svg>',
+    dotIcon = new H.map.Icon(svgMarkup, {anchor: {x:8, y:8}}),
+    group = new  H.map.Group(),
+    i,
+    j;
+
+  // Add a marker for each maneuver
+  for (i = 0;  i < route.leg.length; i += 1) {
+    for (j = 0;  j < route.leg[i].maneuver.length; j += 1) {
+      // Get the next maneuver.
+      maneuver = route.leg[i].maneuver[j];
+      // Add a marker to the maneuvers group
+      var marker =  new H.map.Marker({
+        lat: maneuver.position.latitude,
+        lng: maneuver.position.longitude} ,
+        {icon: dotIcon});
+      marker.instruction = maneuver.instruction;
+      group.addObject(marker);
+    }
+  }
+
+  group.addEventListener('tap', function (evt) {
+    map.setCenter(evt.target.getPosition());
+    openBubble(
+       evt.target.getPosition(), evt.target.instruction);
+  }, false);
+
+  // Add the maneuvers group to the map
+  map.addObject(group);
+}
+
+
+/**
+ * Creates a series of H.map.Marker points from the route and adds them to the map.
+ * @param {Object} route  A route as received from the H.service.RoutingService
+ */
+function addWaypointsToPanel(waypoints){
+
+
+
+  var nodeH4 = document.createElement('h4'),
+    waypointLabels = [],
+    i;
+
+
+   for (i = 0;  i < waypoints.length; i += 1) {
+    waypointLabels.push(waypoints[i].label)
+   }
+
+   nodeH4.textContent = waypointLabels.join(' - ');
+
+  routeInstructionsContainer.innerHTML = '';
+  routeInstructionsContainer.appendChild(nodeH4);
+}
+
+/**
+ * Creates a series of H.map.Marker points from the route and adds them to the map.
+ * @param {Object} route  A route as received from the H.service.RoutingService
+ */
+function addSummaryToPanel(summary){
+  var summaryDiv = document.createElement('div'),
+   content = '<br/>';
+   content += '<b>Distancia total</b>: ' + summary.distance  + ' metros <br/>';
+   content += '<b>Tiempo del recorrido</b>: ' + summary.travelTime.toMMSS(); // + ' (in current traffic)';
+   content += '<br/><br/>';
+
+
+  summaryDiv.style.fontSize = 'large';
+  summaryDiv.style.marginLeft ='15px';
+  summaryDiv.style.marginRight ='15px';
+  summaryDiv.innerHTML = content;
+  routeInstructionsContainer.appendChild(summaryDiv);
+}
+
+/**
+ * Creates a series of H.map.Marker points from the route and adds them to the map.
+ * @param {Object} route  A route as received from the H.service.RoutingService
+ */
+function addManueversToPanel(route){
+
+
+
+  var nodeOL = document.createElement('ol'),
+    i,
+    j;
+
+  nodeOL.style.fontSize = 'medium';
+  nodeOL.style.marginLeft ='15px';
+  nodeOL.style.marginRight ='15px';
+  nodeOL.className = 'directions';
+
+     // Add a marker for each maneuver
+  for (i = 0;  i < route.leg.length; i += 1) {
+    for (j = 0;  j < route.leg[i].maneuver.length; j += 1) {
+      // Get the next maneuver.
+      maneuver = route.leg[i].maneuver[j];
+
+      var li = document.createElement('li'),
+        spanArrow = document.createElement('span'),
+        spanInstruction = document.createElement('span');
+
+      spanArrow.className = 'arrow '  + maneuver.action;
+      spanInstruction.innerHTML = maneuver.instruction;
+      li.appendChild(spanArrow);
+      li.appendChild(spanInstruction);
+
+      nodeOL.appendChild(li);
+    }
+  }
+
+  routeInstructionsContainer.appendChild(nodeOL);
+}
+
+
+Number.prototype.toMMSS = function () {
+  return  Math.floor(this / 60)  +' minutos '; //+ (this % 60)  + ' seg.';
+}
+
+// Now use the map as required...
+//calculateRouteFromAtoB (platform);
+
+
+/**
+ * Calculates and displays the address details of 200 S Mathilda Ave, Sunnyvale, CA
+ * based on a free-form text
+ *
+ *
+ * A full list of available request parameters can be found in the Geocoder API documentation.
+ * see: http://developer.here.com/rest-apis/documentation/geocoder/topics/resource-geocode.html
+ *
+ * @param   {H.service.Platform} platform    A stub class to access HERE services
+ */
+function geocode(platform,direccion) {
+  var geocoder = platform.getGeocodingService(),
+    geocodingParameters = {
+      searchText: direccion,
+      jsonattributes : 1
+    };
+
+  geocoder.geocode(
+    geocodingParameters,
+    geo_onSuccess,
+    geo_onError
+  );
+}
+/**
+ * This function will be called once the Geocoder REST API provides a response
+ * @param  {Object} result          A JSONP object representing the  location(s) found.
+ *
+ * see: http://developer.here.com/rest-apis/documentation/geocoder/topics/resource-type-response-geocode.html
+ */
+function geo_onSuccess(result) {
+  if (result.response.view[0] == undefined) {
+	  M.toast({html: 'Dirección no encontrada!<br/>Intenta escribirla de otra forma.'});
+  } else {
+	  var locations = result.response.view[0].result;
+	  calcular_itinerario(locations);
+  }
+ /*
+  * The styling of the geocoding response on the map is entirely under the developer's control.
+  * A representitive styling can be found the full JS + HTML code of this example
+  * in the functions below:
+  */
+  //addLocationsToMap(locations);
+  //addLocationsToPanel(locations);
+  // ... etc.
+}
+
+/**
+ * This function will be called if a communication error occurs during the JSON-P request
+ * @param  {Object} error  The error message received.
+ */
+function geo_onError(error) {
+  //alert('Ooops!');
+   M.toast({html: 'Ocurrió un error validando la dirección. Por favor, pruebe otra vez.'});
+}
+
+
---
+++ init.js
@@ -0,0 +1,135 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+	$('#mapa').hide();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+var contenido_itinerario = "";
+var direccion = "";
+var id_provincia = 0;
+var lat;
+var lon;
+
+function mostrar_itinerario() {
+	if (id_provincia != 0) calcular_itinerario(null);
+	else if ($('#direccion').val() == undefined || $('#direccion').val() == "") M.toast({html: 'Debe introducirse una dirección de salida.'});
+	else geocode(platform,$('#direccion').val());	
+}
+
+function calcular_itinerario(locations) {
+	
+	var ids_provincias_minube = {"A Coruña":989,"Lugo":1016,"Ourense":1023,"Pontevedra":1025};
+	if (id_provincia == 0) id_provincia = ids_provincias_minube[locations[0].location.address.county];
+	if (direccion == "" ) direccion = locations[0].location.address.label;
+	//locations[0].location.displayPosition.latitude
+	var cat = -1;
+
+	var sun_patterns = ["ADF","ABCDF","EF","ABCF","CEF","ACD","ABCF"];
+	var rain_patterns = ["ADF","ABCDF","AD","ABCF","ACEF","ACD","ABCF"];
+	// TODO: Determinar si hace sol en la ubicación de salida
+
+	if (lat == null) {
+		lat = locations[0].location.displayPosition.latitude;
+		lon = locations[0].location.displayPosition.longitude;
+	}
+	var dist = 1000;
+	var api_key = '<MINUBE API KEY>';
+	
+	var contenido_itinerario = '<div class="row"><div class="col s12"><div class="route-header">Itinerario desde '+direccion+'<div></div></div>';
+	 
+	cont = 0;
+	items = false;
+	for (p in sun_patterns) {
+		cat = -1;
+		cont++;
+		for (var i in params) {
+			if (params[i].pattern == sun_patterns[p]) {
+				cat = params[i].category_id;
+			}						
+		}
+		if (cat > -1) {
+			console.log("minube_proxy.php?url="+encodeURIComponent("http://papi.minube.com/pois?lang=es&country_id=63&zone_id="+id_provincia+"&subcategory_id="+cat+"&latitude="+lat+"&longitude="+lon+"&max_distance="+dist+"&min_distance=0&page=0&api_key="+api_key));
+			$.ajax({type: "GET",url: "minube_proxy.php?url="+encodeURIComponent("http://papi.minube.com/pois?lang=es&country_id=63&zone_id="+id_provincia+"&subcategory_id="+cat+"&latitude="+lat+"&longitude="+lon+"&max_distance="+dist+"&min_distance=0&page=0&api_key="+api_key), async: false})
+			.done(function(data) {
+				var pois = data;
+				if (data.length == 0) {					
+					//contenido_itinerario += '<div class="row"><div class="col s12 center"><h5>Lo siento, no se han encontrado puntos de interés en la ubicación indicada</h5></div>';
+					contenido_itinerario += '<div class="row">';
+					contenido_itinerario += '	  <div class="col s2 center route-line"><div class="route-number">'+cont+'</div></div>';
+					contenido_itinerario += '	  <div class="col s5 center"><div class="route-title">No hay puntos de interés del tipo deseado</div></div>';
+					contenido_itinerario += '	  <div class="col s5 center"><img src="/images/no-image.png" class="z-depth-3" style="max-width: 100%"/></div>';
+					contenido_itinerario += '	</div>';
+					//console.log(pois[j].name+" lat: "+pois[j].latitude+", lon: "+pois[j].longitude);					
+				}else items = true;
+
+				for (var j in pois) {
+					contenido_itinerario += '<div class="row">';
+					contenido_itinerario += '	  <div class="col s2 center route-line"><div class="route-number">'+cont+'</div></div>';
+					contenido_itinerario += '	  <div class="col s5 center"><div class="route-title">'+pois[j].name+'</div></div>';
+					contenido_itinerario += '	  <div class="col s5 center"><img src="'+pois[j].picture_url+'" class="z-depth-3" style="max-width: 100%"/></div>';
+					contenido_itinerario += '	</div>';
+					console.log(pois[j].name+" lat: "+pois[j].latitude+", lon: "+pois[j].longitude);
+					break; // Solo queremos uno (deberia ser el mas relevante)
+				}
+			})
+			.fail(function() {
+				var contenido_itinerario = '<div class="row"><div class="col s12"><div class="route-header red-text">Error buscando puntos de interés!<div></div></div>';
+				$('#contenido').html(contenido_itinerario);	
+				id_provincia = 0;
+				direccion = "";
+				lat = null;		
+			})	
+				
+		}
+	}
+	if (items) contenido_itinerario += '<a href="javascript:void(0);" onclick="mostrar_mapa()" class="btn-floating btn-large waves-effect waves-light cyan darken-3 right map-button pulse"><i class="material-icons">map</i></a>';
+	$('#contenido').html(contenido_itinerario);
+
+	//id_provincia = 0;
+	//direccion = "";
+	//lat = null;
+}
+
+var contenido_mapa = "";
+
+function mostrar_mapa() {
+	if (contenido_mapa == undefined || contenido_mapa == "") {
+		var contenido_mapa =  `
+			 <a href="javascript:void(0);" onclick="mostrar_itinerario()" class="btn-floating btn-large waves-effect waves-light cyan darken-3 right map-button pulse"><i class="material-icons">timeline</i></a>			
+			<div id="map" style="width: 100%; height: 350px; background: grey"></div>
+			<div id="panel" style="width: 100%;"></div>			
+			<!-- <script type="text/javascript" src='js/app.js'></script> -->
+		`;
+		$('#contenido').html(contenido_mapa);
+		/////////////////////////////////////////////////////////////
+		// set up containers for the map  + panel
+		mapContainer = document.getElementById('map'),
+		  routeInstructionsContainer = document.getElementById('panel');
+
+		defaultLayers = platform.createDefaultLayers();
+
+		//Step 2: initialize a map - this map is centered over Berlin
+		map = new H.Map(mapContainer,
+		  defaultLayers.normal.map,{
+		  center: {lat:42.22133, lng:-8.73334},
+		  zoom: 13
+		});
+
+		//Step 3: make the map interactive
+		// MapEvents enables the event system
+		// Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+		behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+
+		// Create the default UI components
+		ui = H.ui.UI.createDefault(map, defaultLayers);
+
+		/////////////////////////////////////////////////////////////////
+
+		calculateRouteFromAtoB(platform);
+	}else {
+		$('#contenido').html(contenido_mapa);
+		calculateRouteFromAtoB(platform);
+	}
+}
